fb76e295723256c3b2811e68c8a56092714eb782
synbyte <rashnikov2.0@gmail.com>
2024-09-22 21:39:45 -0700

    added Connect features. Need to work on collecting payment correctly. Then fleshing out onboarding better. Then dashboard.

--- givevery/app/layout.tsx
+++ givevery/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
--- givevery/components/env-var-warning.tsx
+++ givevery/components/env-var-warning.tsx
@@ -6,7 +6,7 @@ export function EnvVarWarning() {
   return (
     <div className="flex gap-4 items-center">
       <Badge variant={"outline"} className="font-normal">
-        Supabase environment variables required
+        Supabase environment variables required!!
       </Badge>
       <div className="flex gap-2">
         <Button
