8c57cf68bccf986d34d8eeda525dea859c16e13f
synbyte <rashnikov2.0@gmail.com>
2024-09-23 01:46:35 +0000

    installed

---
+++ givevery/app/api/create-payment-intent/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-11-15',
+});
+
+export async function POST(request) {
+  const { amount } = await request.json();
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100, // amount in cents
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      }
+    });
+
+    console.log('payment succeeded!')
+    return NextResponse.json({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
---
+++ givevery/app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/hero";
+import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
+import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
+import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+
+export default async function Index() {
+  return (
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <p>Sign up</p>
+      </main>
+    </>
+  );
+}
