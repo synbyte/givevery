2b5291dab27477541a4c99ad7738bbd0cb467d18
synbyte <rashnikov2.0@gmail.com>
2024-11-04 19:06:19 -0800

    removed navbar from auth pages, removed redirect in next.config

--- givevery/app/(auth-pages)/layout.tsx
+++ givevery/app/(auth-pages)/layout.tsx
@@ -37,19 +37,19 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <main className="min-h-screen flex items-start">
-            
+
             <div className=" w-full flex flex-col  items-center">
-              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+              {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
-                      
+
                     </div>
                   </div>
                   {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                 </div>
-              </nav>
+              </nav> */}
               <div className="flex flex-col gap-20 w-full p-5">
                 {children}
               </div>
--- givevery/next.config.js
+++ givevery/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    redirects: async () => {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                permanent: true,
-            },
-        ];
-    }
+
 };
 
 module.exports = nextConfig;
