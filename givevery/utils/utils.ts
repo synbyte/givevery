13de2d8350dfb4949d6417c1cc4cb1304445644c
synbyte <rashnikov2.0@gmail.com>
2024-11-04 18:42:45 -0800

    fixes

--- givevery/utils/utils.ts
+++ givevery/utils/utils.ts
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2024-10-28',
 });
 
 /**
